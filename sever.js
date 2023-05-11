02e8ac30d13c460f41dc2d448caadb64f7a6c5dd
DuyXoX <129503395+DuyXoX@users.noreply.github.com>
2023-05-11 14:14:38 +0700

    Add files via upload

--- sever.js
+++ sever.js
@@ -1,27 +1,32 @@
 
-const express = require('express');
-const mongoose = require('mongoose');
-// const userRouter = require('./routes/userRoute');
-const bodyParser = require('body-parser');
-const expressHandlebars = require('express-handlebars');
-const userController = require('./controller/userController')
-const url = "mongodb+srv://duyvmpk:duy123@cluster0.wy2cqmu.mongodb.net/MonggoDB?retryWrites=true&w=majority";
+const express = require("express");
+// const mongoose = require('mongoose');
+// // const userRouter = require('./routes/userRoute');
+// const bodyParser = require('body-parser');
+// const expressHandlebars = require('express-handlebars');
+// const userController = require('./controller/userController')
+// const url = "mongodb+srv://duyvmpk:duy123@cluster0.wy2cqmu.mongodb.net/MonggoDB?retryWrites=true&w=majority";
 
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.engine('.hbs', expressHandlebars.engine());
-app.set('view engine', '.hbs');
-app.use(express.json());
+const port = 3000
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
 
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+// app.use(bodyParser.json());
+// app.engine('.hbs', expressHandlebars.engine());
+// app.set('view engine', '.hbs');
+// app.use(express.json());
 
-app.use('/login', userController);
+// mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+
+// app.use('/login', userController);
 
 
-app.use('/user', userController);
-app.listen(3000, () => {
-    console.log('server is running!')
+// app.use('/user', userController);
+app.get("/", (req, res) => {
+    return res.send("Hello web");
+});
+app.listen(port, () => {
+    console.log('server is running!');
 })
