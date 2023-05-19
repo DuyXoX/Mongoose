3f7d8a040e29ddd69a955086538f9b6988946557
Minh Duy <vminhduy615@gmail.com>
2023-05-19 19:28:41 +0700

    update

--- sever.js
+++ sever.js
@@ -1,27 +1,33 @@
-
-const express = require('express');
-const mongoose = require('mongoose');
-// const userRouter = require('./routes/userRoute');
-const bodyParser = require('body-parser');
+const express = require("express");
+// const mongoose = require('mongoose');
+// // const userRouter = require('./routes/userRoute');
+// const bodyParser = require('body-parser');
 const expressHandlebars = require('express-handlebars');
-const userController = require('./controller/userController')
-const url = "mongodb+srv://duyvmpk:duy123@cluster0.wy2cqmu.mongodb.net/MonggoDB?retryWrites=true&w=majority";
+// const userController = require('./controller/userController')
+// const url = "mongodb+srv://duyvmpk:duy123@cluster0.wy2cqmu.mongodb.net/MonggoDB?retryWrites=true&w=majority";
 
 const app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-app.engine('.hbs', expressHandlebars.engine());
+const port = 3000;
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+// app.use(bodyParser.json());
+// app.engine('.hbs', expressHandlebars.engine());
+app.engine('.hbs', expressHandlebars.engine({extname:'.hbs',defaultlayout:'main'}));
+
 app.set('view engine', '.hbs');
-app.use(express.json());
+// app.use(express.json());
 
-mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+// mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
 
-app.use('/login', userController);
+// app.use('/login', userController);
+// app.use('/user', userController);
 
-
-app.use('/user', userController);
-app.listen(3000, () => {
-    console.log('server is running!')
-})
+app.get('/', (req, res) => {
+    // return res.send("Hello web");
+    return res.render("home");
+});
+app.listen(port, () => {
+    console.log('server is running!');
+});
